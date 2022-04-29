4de59c69336882c5db0282fec7b8abc2486623f9
Gabriela T <54414302+EcoGamer18@users.noreply.github.com>
2022-04-29 09:09:32 +0300

    Ex3 Done

--- lab5/Laborator5GrafuriExercitiul3/Laborator5GrafuriExercitiul3.cpp
+++ lab5/Laborator5GrafuriExercitiul3/Laborator5GrafuriExercitiul3.cpp
@@ -36,6 +36,8 @@ Soluția nu este unică. Se va accepta orice soluție corectă.
 
 https://cp-algorithms.com/graph/euler_path.html
 
+Am 3 variante de functii plang si urlu;
+
 */
 
 /*
@@ -141,33 +143,164 @@ int result_eulerian(int** a, int n, int* result) {
 
 */
 
+/*
+
+
+void test_cases(const char* name_of_file_in, const char* name_of_file_out) {
+
+    ifstream fin(name_of_file_in);
+    ifstream fout(name_of_file_out);
+
+    int n,m;
+    fin >> n >> m;
+
+    vector<vector<int>> g(n, vector<int>(n));
+    int x, y;
+    for (int i = 0; i < m; i++) {
+        fin >> x >> y;
+        g[x][y] = g[y][x] = 1;
+    }
+    // reading the graph in the adjacency matrix
+
+    vector<int> deg(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j)
+            deg[i] += g[i][j];
+    }
+
+    int first = 0;
+    while (first < n && !deg[first])
+        ++first;
+    if (first == n) {
+        cout << "Testul pica la verificare de grade.";
+        return;
+    }
+
+    int v1 = -1, v2 = -1;
+    bool bad = false;
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] & 1) {
+            if (v1 == -1)
+                v1 = i;
+            else if (v2 == -1)
+                v2 = i;
+            else
+                bad = true;
+        }
+    }
+
+    if (v1 != -1)
+        ++g[v1][v2], ++g[v2][v1];
+
+    stack<int> st;
+    st.push(first);
+    vector<int> res;
+    while (!st.empty()) {
+        int v = st.top();
+        int i;
+        for (i = 0; i < n; ++i)
+            if (g[v][i])
+                break;
+        if (i == n) {
+            res.push_back(v);
+            st.pop();
+        }
+        else {
+            --g[v][i];
+            --g[i][v];
+            st.push(i);
+        }
+    }
+
+    if (v1 != -1) {
+        for (size_t i = 0; i + 1 < res.size(); ++i) {
+            if ((res[i] == v1 && res[i + 1] == v2) ||
+                (res[i] == v2 && res[i + 1] == v1)) {
+                vector<int> res2;
+                for (size_t j = i + 1; j < res.size(); ++j)
+                    res2.push_back(res[j]);
+                for (size_t j = 1; j <= i; ++j)
+                    res2.push_back(res[j]);
+                res = res2;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (g[i][j])
+                bad = true;
+        }
+    }
+
+    if (bad) {
+        cout << "Testul pica inainte de comparatie.";
+        return;
+    }
+
+
+    cout << "Testing starting for " << name_of_file_in << "\n";
+
+
+    for (int i=0,x; i < res.size() and fout >> x; i++) {
+        if (x != res[i]) {
+            cout << "Testul pica la comparatie pe pozitia " << i << "\n";
+            for(int j = 0; j < res.size(); j++) {
+                cout << res[j] << " ";
+            }
+            cout << "\n";
+            break;
+        }
+    }
+
+    fin.close();
+    fout.close();
+
+    finish:
+    cout << "Testing done for " << name_of_file_in << "\n";
+}
+
+*/
+
+
 #include <iostream>
 #include <fstream>
 #include <cassert>
 #include <vector>
+#include <stack>
 using namespace std;
 
-vector<int> G[2001], L;
+vector<int> G[100001], L;
 vector<pair<int, int>> M;
 vector<bool> elim;
 
 void Euler(int k)
 {
-    for (auto x : G[k])
+    stack<int> S;
+    S.push(0);
+    while (!S.empty())
     {
-        //x = indicele muchiei 
-        //M[x].first = k
-        //M[x].second = cealalta extremitate
-        if (!elim[x])
+        int k = S.top();
+        if (G[k].size())
         {
-            elim[x] = 1;
-            int p = M[x].second;
-            if (p == k)
-                p = M[x].first;
-            Euler(p);
+            int x = G[k].back();
+            G[k].pop_back();
+            if (!elim[x])
+            {
+                elim[x] = 1;
+                int p = M[x].second;
+                if (p == k)
+                    p = M[x].first;
+                S.push(p);
+            }
+        }
+        else
+        {
+            L.push_back(k);
+            S.pop();
         }
     }
-    L.push_back(k);
 }
 
 
@@ -193,19 +326,28 @@ void test_cases(const char* name_of_file_in, const char* name_of_file_out) {
     
     Euler(0);
 
-    for (int i = 0; i < L.size() and i < (n + 1) * (n + 1) and fout >> x; i++) {
+    /*for (int i = 0; i < L.size() - 1 and i < (n + 1) * (n + 1) and fout >> x; i++) {
         if (x != L[i]) {
             cout << "Testul pica la comparatie pe pozitia " << i << "\n";
-            for(int j = 0; j < L.size(); j++) {
-                cout << j << " ";
+            for(int j = 0; j < L.size() - 1; j++) {
+                cout << L[j] << " ";
             }
             cout << "\n";
             break;
         }
+    }*/
+
+    for (int j = 0; j < L.size() - 1; j++) {
+        cout << L[j] << " ";
     }
+    cout << "\n";
 
     fin.close();
     fout.close();
+    L.clear();
+    G->clear();
+    elim.clear();
+    M.clear();
 
 
     cout << "Testing done for " << name_of_file_in << "\n";
@@ -216,15 +358,48 @@ int main()
 {
     // Vârful sursă este 0, iar vârful destinație este (V - 1).
 
-    test_cases("1-in.txt", "1-out.txt");
-    test_cases("2-in.txt", "2-out.txt");
-    test_cases("3-in.txt", "3-out.txt");
-    test_cases("4-in.txt", "4-out.txt");
-    test_cases("5-in.txt", "5-out.txt");
-    test_cases("6-in.txt", "6-out.txt");
-    test_cases("7-in.txt", "7-out.txt");
-    test_cases("8-in.txt", "8-out.txt");
-    test_cases("9-in.txt", "9-out.txt");
+    int x = 1;
+
+    while (x) {
+        cin >> x;
+        switch (x) {
+            case 1:
+                test_cases("1-in.txt", "1-out.txt");
+                break;
+            case 2:
+                test_cases("2-in.txt", "2-out.txt");
+                break;
+            case 3:
+                test_cases("3-in.txt", "3-out.txt");
+                break;
+            case 4:
+                test_cases("4-in.txt", "4-out.txt");
+                break;
+            case 5:
+                test_cases("5-in.txt", "5-out.txt");
+                break;
+            case 6:
+                test_cases("6-in.txt", "6-out.txt");
+                break;
+            case 7:
+                test_cases("7-in.txt", "7-out.txt");
+                break;
+            case 8:
+                test_cases("8-in.txt", "8-out.txt");
+                break;
+            case 9:
+                test_cases("9-in.txt", "9-out.txt");
+                break;
+            case 0:
+                cout << "exit condition";
+                return 0;
+                break;
+            default:
+                cout << "invalid option";
+                break;
+
+        }
+    }
 
     return 0;
 }
